87b1f37e007eccc99c321408e5720037b402c807
Joey Hinckley <JoeyHinckley34@gmail.com>
2023-10-20 12:11:30 -0400

    added main to 121

--- 121/121.cpp
+++ 121/121.cpp
@@ -10,7 +10,7 @@ public:
         int min_price = prices[0];
         int maxprof = 0;
 
-        for(int i=1;i<prices.size();i++){
+        for(unsigned long int i=1;i<prices.size();i++){
             maxprof = max(maxprof,prices[i]-min_price);
             min_price = min(prices[i],min_price);
         }
@@ -18,3 +18,12 @@ public:
         return maxprof;
     }
 };
+
+
+int main(){
+    Solution a;
+    vector<int> v = {7,1,5,3,6,4};
+    int m = a.maxProfit(v);
+    cout << m << endl;
+    return 0;
+}
